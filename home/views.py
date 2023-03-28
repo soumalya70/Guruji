543032cf3ff07b3473e2c802906ef9496cc1a792
soumalya70 <soumalyasaha200@gmail.com>
2023-03-28 23:28:46 +0530

    deployed4.0

--- home/views.py
+++ home/views.py
@@ -1,22 +1,22 @@
 from rest_framework import generics
 from .models import *
 from .serializers import *
-class BannerImageAPIView(generics.ListAPIView):
+class BannerImageAPIView(generics.ListCreateAPIView):
     queryset = BannerImage.objects.all().order_by('-id')[:1]
     serializer_class = BannerImageSerializer
 
-class AboutGurujiAPIView(generics.ListAPIView):
+class AboutGurujiAPIView(generics.ListCreateAPIView):
     queryset = AboutGuruji.objects.all().order_by('-id')[:1]
     serializer_class = AboutGurujiSerializer
 
-class OurServiceAPIView(generics.ListAPIView):
+class OurServiceAPIView(generics.ListCreateAPIView):
     queryset = Our_Services_Cards.objects.all()
     serializer_class = OurservicesCardsSerializer
 
-class ResolveProblemsAPIView(generics.ListAPIView):
+class ResolveProblemsAPIView(generics.ListCreateAPIView):
     queryset = ResolveProblem.objects.all().order_by('-id')[:1]
     serializer_class=ResolveProblemsSerializer
     
-class GurujWordsAPIView(generics.ListAPIView):
+class GurujWordsAPIView(generics.ListCreateAPIView):
     queryset =GurujiWords.objects.all().order_by('-id')[:3]
     serializer_class=GurujiWordsSerializer
